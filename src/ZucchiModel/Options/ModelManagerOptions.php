e6a69bf313e1420eaa86a466b7017679c74d2ea7
Matt Cockayne <matt@zucchi.co.uk>
2013-03-11 19:22:32 +0000

    updated config

---
+++ src/ZucchiModel/Options/ModelManagerOptions.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * ZucchiSecurity (http://zucchi.co.uk/)
+ *
+ * @link      http://github.com/zucchi/ZucchiSecurity for the canonical source repository
+ * @copyright Copyright (c) 2005-2012 Zucchi Limited (http://zucchi.co.uk)
+ * @license   http://zucchi.co.uk/legals/bsd-license New BSD License
+ */
+namespace ZucchiModel\Options;
+
+use Zend\Stdlib\AbstractOptions;
+
+/**
+ * 
+ * @author Matt Cockayne <matt@zucchi.co.uk>
+ * @package ZucchiSecurity 
+ * @subpackage Auth 
+ * @category Plugin/Options
+ */
+class ModelManagerOptions extends AbstractOptions
+{
+
+}
