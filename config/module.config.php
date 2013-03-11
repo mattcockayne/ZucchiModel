d191265c29d02d1d30f392b47c39b887b9212e1b
Matt Cockayne <matt@zucchi.co.uk>
2013-03-11 19:25:14 +0000

    updated module.config

--- config/module.config.php
+++ config/module.config.php
@@ -3,7 +3,7 @@ return array(
     'service_manager' => array(
         'factories' => array(
             'zucchimodel.modelmanager' => function ($sm) {
-                $manager = new ZucchiModel\ModelManager();
+                $manager = new ZucchiModel\ModelManager($sm->get('Zend\Db\Adapter\Adapter'));
                 return $manager;
             },
             'Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory',
