77636e4ec3cb0c214ab1909147c6ba9b6dadb8f4
Richard Nicol <richardnicol71@gmail.com>
2013-03-21 12:49:03 +0000

    Add findOne logic to ModelManager.

--- src/ZucchiModel/ModelManager.php
+++ src/ZucchiModel/ModelManager.php
@@ -9,6 +9,8 @@
 namespace ZucchiModel;
 
 use Zend\Db\Adapter\AdapterInterface;
+use Zend\Db\Adapter\Driver\ResultInterface;
+use Zend\Db\ResultSet\HydratingResultSet;
 use Zend\Code\Annotation\AnnotationManager;
 use Zend\Code\Annotation\Parser;
 use Zend\Code\Reflection\ClassReflection;
@@ -17,7 +19,7 @@ use Zend\EventManager\Event;
 use Zend\EventManager\EventManagerInterface;
 use Zend\EventManager\EventManagerAwareInterface;
 use Zend\EventManager\EventManagerAwareTrait;
-use Zend\Stdlib\Hydrator;
+use ZucchiModel\Hydrator;
 use ZucchiModel\Annotation\MetadataListener;
 use ZucchiModel\Metadata;
 use ZucchiModel\Query\Criteria;
@@ -317,7 +319,8 @@ class ModelManager implements EventManagerAwareInterface
     // TODO: test compound keys
     // TODO: take into account schema and table names in foreignKeys
     // TODO: store results in mapCache
-    // TODO: if one dataSource, but more are need for all fields throw error.
+    // TODO: break out sql into its own driver(matt may change this) so we can add NoSql etc.
+    // TODO: add function with factory to new sql driver
     public function findOne(Criteria $criteria)
     {
         $select = $this->sql->select();
@@ -332,90 +335,129 @@ class ModelManager implements EventManagerAwareInterface
         $metadata = $this->getMetadata($model);
 
         // Check dataSource and metadata exist
-        if (!isset($metadata['dataSourceMetadata']) || !isset($metadata['model']['dataSource'])) {
+        if (!isset($metadata['dataSourceMetadata']) || empty($metadata['dataSourceMetadata'])) {
             throw new \RuntimeException(sprintf('No Data Source Metadata can be found for this Model. %s given.', var_export($model, true)));
         }
 
-        $dataSourceMetadata = $metadata['dataSourceMetadata'];
-        $dataSources = $metadata['model']['dataSource'];
+        // List of Data Source Names
+        $dataSources = array();
 
+        // Fields to select
+        $selectColumns = array();
+
+        // Create a look up for all the foreign keys
         $foreignKeys = array();
-        foreach ($dataSources as $dataSource) {
-            $constraints = $dataSourceMetadata[$dataSource]->getConstraints();
-            foreach ($constraints as $constraint) {
-                if ($constraint->isForeignKey()) {
+
+        foreach ($metadata['dataSourceMetadata'] as $dataSource => $metadata) {
+            // Create list of Data Sources
+            $dataSources[] = $dataSource;
+
+            // Build up an array of all the Columns to select
+            $columns = $metadata->getColumns();
+            array_walk(
+                $columns,
+                function ($column) use (&$selectColumns, $dataSource) {
+                    if (!isset($selectColumns[$column->getName()])) {
+                        $selectColumns[$column->getName()] = $dataSource;
+                    }
+                }
+            );
+
+            // Build up an array of all the Foreign Key Relationships
+            $constraints = $metadata->getConstraints();
+            array_walk(
+                $constraints,
+                function ($constraint) use (&$foreignKeys) {
                     $foreignKeys[$constraint->getReferencedTableName()] = array(
                         'tableName' => $constraint->getTableName(),
                         'columns' => $constraint->getColumns(),
                         'referencedColumns' => $constraint->getReferencedColumns()
                     );
                 }
+            );
+        }
+
+        // Get the first Data Source which will be the From Table
+        $from = array_shift($dataSources);
+
+        // Create lookup to match Table name to alias
+        $tableNameLookup = array($from => 0);
+
+        // Set form Table and Columns if present
+        $select->from(array('t0' => $from));
+        $columns = array_keys($selectColumns, $from);
+        if (!empty($columns)) {
+            $select->columns($columns);
+        }
+
+        $joins = array();
+
+        // Building Join references
+        foreach ($dataSources as $referencedTableName) {
+            // Make sure required Metadata is present
+            if (empty($foreignKeys[$referencedTableName]['tableName']) ||
+                empty($foreignKeys[$referencedTableName]['columns']) ||
+                empty($foreignKeys[$referencedTableName]['referencedColumns']) ||
+                (count($foreignKeys[$referencedTableName]['columns']) != count($foreignKeys[$referencedTableName]['referencedColumns']))
+            ) {
+                throw new \RuntimeException(sprintf('Invalid Foreign Key Metadata defined for %s.', $referencedTableName));
+            }
+
+            $tableName = $foreignKeys[$referencedTableName]['tableName'];
+            $columns = $foreignKeys[$referencedTableName]['columns'];
+            $referencedColumns = $foreignKeys[$referencedTableName]['referencedColumns'];
+
+            // If not used before add table to temporary lookup
+            if (!isset($tableNameLookup[$tableName])) {
+                $tableNameLookup[$tableName] = count($tableNameLookup);
+            }
+            $tableFrom = $tableNameLookup[$tableName];
+
+            // If referenced table has not used before add table to temporary lookup
+            if (!isset($tableNameLookup[$referencedTableName])) {
+                $tableNameLookup[$referencedTableName] = count($tableNameLookup);
+            }
+            $tableTo = $tableNameLookup[$referencedTableName];
+
+            // Create array of map on for join
+            $on = array();
+            for ($i = 0; $i < count($columns); $i++) {
+                $on[] = 't' . $tableFrom . '.' . $columns[$i] . ' = t' . $tableTo . '.' . $referencedColumns[$i];
+            }
+
+            // Find all columns to "select" for this join
+            $columns = array_keys($selectColumns, $referencedTableName);
+
+            // Setting join reference
+            $joins[$tableTo] = array(
+                'table' => array('t' . $tableTo => $referencedTableName),
+                'on' => implode(' AND ', $on),
+                'columns' => (!empty($columns)) ? $columns : array()
+            );
+        }
+
+        // Check if we have any joins
+        if (!empty($joins)) {
+            // Sort into table join order e.g. t0, t1, t2
+            ksort($joins);
+
+            // Add joins for other Data Sources
+            foreach($joins as $join) {
+                $select->join($join['table'], $join['on'], $join['columns'], 'left');
             }
         }
 
-        // Get the first dataSource which will be the from table
-        if ($from = array_shift($dataSources)) {
-            $tableNameLookup = array($from => 't0');
-            $select->from(array('t0' => $from));
-
-            // Join subsequent table names
-            foreach ($dataSources as $referencedTableName) {
-                // Check a foreign key to match exists
-                if (!isset($foreignKeys[$referencedTableName])) {
-                    throw new \RuntimeException(sprintf('No Foreign Keys were found to construct join with %s.', $referencedTableName));
-                }
-
-                // Check there is a table name to join on
-                if (empty($foreignKeys[$referencedTableName]['tableName'])) {
-                    throw new \RuntimeException(sprintf('No Table Name was found to construct join with %s.', $referencedTableName));
-                }
-
-                // Check there are columns to join on
-                if (empty($foreignKeys[$referencedTableName]['columns']) || empty($foreignKeys[$referencedTableName]['columns'])) {
-                    throw new \RuntimeException(sprintf('No Columns were found to construct join with %s.', $referencedTableName));
-                }
-
-                $tableName = $foreignKeys[$referencedTableName]['tableName'];
-                $columns = $foreignKeys[$referencedTableName]['columns'];
-                $referencedColumns = $foreignKeys[$referencedTableName]['referencedColumns'];
-
-                // Make sure the columns match with referencedColumns
-                if (count($columns) != count($referencedColumns)) {
-                    throw new \RuntimeException(sprintf('Failed to construct join with %s. Columns did not match Referenced Columns', $referencedTableName));
-                }
-
-                if (!isset($tableNameLookup[$tableName])) {
-                    $tableNameLookup[$tableName] = 't'.count($tableNameLookup);
-                }
-
-                $tableFrom = $tableNameLookup[$tableName];
-
-                if (!isset($tableNameLookup[$referencedTableName])) {
-                    $tableNameLookup[$referencedTableName] = 't'.count($tableNameLookup);
-                }
-                $tableTo = $tableNameLookup[$referencedTableName];
-
-                $on = array();
-                for ($i=0; $i < count($columns); $i++) {
-                    $on[] = $tableFrom.'.'.$columns[$i].' = '.$tableTo.'.'.$referencedColumns[$i];
-                }
-
-                // Add joins for other data sources
-                $select->join(array($tableTo => $referencedTableName), implode(' AND ', $on), '*', 'left');
-            }
-        }
-
-        // Check and apply any where
+        // Check and apply any "where"
         if ($where = $criteria->getWhere()) {
             $select->where($where);
         }
 
-        // Check and apply any offset
+        // Check and apply any "offset"
         if ($offset = $criteria->getOffset()) {
             $select->offset($offset);
         }
 
-        // Force limit to one
+        // Force "limit" to one
         $select->limit(1);
 
         // Build and run the DB statement
