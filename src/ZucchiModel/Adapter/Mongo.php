e71d8409570e0ce92e62d7028975efaa15022fb6
Matt Cockayne <matt@zucchi.co.uk>
2013-03-24 21:22:58 +0000

    added skeleton for Mongo Adapter

---
+++ src/ZucchiModel/Adapter/Mongo.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Mongo.php - (http://zucchi.co.uk) 
+ *
+ * @link      http://github.com/zucchi/{PROJECT_NAME} for the canonical source repository
+ * @copyright Copyright (c) 2005-2013 Zucchi Limited. (http://zucchi.co.uk)
+ * @license   http://zucchi.co.uk/legals/bsd-license New BSD License
+ */
+
+namespace ZucchiModel\Adapter;
+
+/**
+ * Mongo
+ *
+ * Description of class
+ *
+ * @author Matt Cockayne <matt@zucchi.co.uk>
+ * @package ZucchiModel\Adapter
+ * @subpackage 
+ * @category 
+ */
+class Mongo extends AbstractAdapter
+{
+    /**
+     * retrieve metadata for class
+     * @param $class
+     * @return mixed
+     */
+    public function getMetaData($class)
+    {
+
+    }
+
+    /**
+     * build and return query object from criteria
+     *
+     * @param Criteria $criteria
+     * @param Array $metadata
+     * @return mixed
+     */
+    public function buildQuery(Criteria $criteria, Array $metadata)
+    {
+
+    }
+
+    /**
+     * execute supplied query and return result
+     * @param $query
+     * @return mixed
+     */
+    public function execute($query)
+    {
+
+    }
+}
