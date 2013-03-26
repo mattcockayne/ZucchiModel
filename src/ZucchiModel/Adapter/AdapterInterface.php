7854c45bb241cc8ae3e48750e9c4f0b8f5656db7
Richard Nicol <richardnicol71@gmail.com>
2013-03-26 10:09:08 +0000

    Remove unused getHydratingResultSet.

--- src/ZucchiModel/Adapter/AdapterInterface.php
+++ src/ZucchiModel/Adapter/AdapterInterface.php
@@ -48,13 +48,4 @@ interface AdapterInterface
      * @return mixed
      */
     public function execute($query);
-
-    /**
-     * return a data source specific hydrating rowset
-     *
-     * @param $hydrator
-     * @param $model
-     * @return mixed
-     */
-    public function getHydratingResultSet($hydrator, $model);
 }
