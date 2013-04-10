2ac3139aa7bff60a88be863ef25ae0dd840e0167
Richard Nicol!@)$€#¡¡^^^!6161 <richardnicol71@gmail.com>
2013-04-10 14:29:29 +0100

    Add test objects for Annotation testing.

---
+++ tests/unit/ZucchiModelTest/Annotation/TestAsset/User.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * ZucchiModel (http://zucchi.co.uk)
+ *
+ * @link      http://github.com/zucchi/ZucchiModel for the canonical source repository
+ * @copyright Copyright (c) 2005-2013 Zucchi Limited. (http://zucchi.co.uk)
+ * @license   http://zucchi.co.uk/legals/bsd-license New BSD License
+ */
+namespace ZucchiModelTest\Annotation\TestAsset;
+
+use ZucchiModel\Annotation as Model;
+use ZucchiModel\Behaviour;
+/**
+ * @author Rick Nicol <rick@zucchi.co.uk>
+ * @Model\Target({"moduledev_user"})
+ * @Model\Relationship({"name": "Roles", "model": "Application\Entity\Role", "type": "ManytoMany", "mappedKey": "id", "mappedBy": "User_id", "foreignKey": "id", "foreignBy": "Role_id", "referencedBy": "moduledev_user_role", "referencedOrder": "sort"})
+ */
+class User
+{
+    use Behaviour\IdentityTrait;
+    use Behaviour\TimestampTrait;
+    use Behaviour\ChangeTrackingTrait;
+
+    /**
+     * users forename
+     *
+     * @Model\Field("string")
+     */
+    public $forename;
+
+    /**
+     * users surname
+     *
+     * @Model\Field("string")
+     */
+    public $surname;
+
+    /**
+     * users emaiol address
+     *
+     * @Model\Field("string")
+     */
+    public $email;
+}
