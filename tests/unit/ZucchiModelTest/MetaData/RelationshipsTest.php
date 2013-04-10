480917141c6aebed0cc7bad71909e4041111e2cc
Richard Nicol!@)$€#¡¡^^^!6161 <richardnicol71@gmail.com>
2013-04-10 12:24:48 +0100

    Change Tests to seperate namespace ZucchiModelTest.

--- tests/unit/ZucchiModelTest/MetaData/RelationshipsTest.php
+++ tests/unit/ZucchiModelTest/MetaData/RelationshipsTest.php
@@ -1,5 +1,12 @@
 <?php
-namespace ZucchiModel;
+/**
+ * ZucchiModel (http://zucchi.co.uk)
+ *
+ * @link      http://github.com/zucchi/ZucchiModel for the canonical source repository
+ * @copyright Copyright (c) 2005-2013 Zucchi Limited. (http://zucchi.co.uk)
+ * @license   http://zucchi.co.uk/legals/bsd-license New BSD License
+ */
+namespace ZucchiModelTest;
 use Codeception\Util\Stub;
 
 class MetaDataRelationshipsTest extends \Codeception\TestCase\Test
