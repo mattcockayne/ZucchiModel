70300b5107352855aa831e85b05a80c7e54d9052
Richard Nicol <richardnicol71@gmail.com>
2013-04-11 11:48:12 +0100

    Add Role to Adapter\TestAsset

---
+++ tests/unit/ZucchiModelTest/Adapter/TestAsset/Role.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * ZucchiModel (http://zucchi.co.uk)
+ *
+ * @link      http://github.com/zucchi/ZucchiModel for the canonical source repository
+ * @copyright Copyright (c) 2005-2013 Zucchi Limited. (http://zucchi.co.uk)
+ * @license   http://zucchi.co.uk/legals/bsd-license New BSD License
+ */
+namespace ZucchiModelTest\Adapter\TestAsset;
+
+use ZucchiModel\Annotation as Model;
+use ZucchiModel\Behaviour;
+
+/**
+ * @author Rick Nicol <rick@zucchi.co.uk>
+ * @Model\Target({"moduledev_role"})
+ */
+class Role {
+    use Behaviour\IdentityTrait;
+
+    /**
+     * Role name
+     *
+     * @Model\Field("string")
+     */
+    public $name;
+}
