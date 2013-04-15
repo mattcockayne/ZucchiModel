0368fa9c76bc3401c986fd35825bc089baf2353c
Richard Nicol <richardnicol71@gmail.com>
2013-04-15 15:44:53 +0100

    Add Test Classes to Behaviour.

---
+++ tests/unit/ZucchiModelTest/Behaviour/TestAsset/Customer.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * ZucchiModel (http://zucchi.co.uk)
+ *
+ * @link      http://github.com/zucchi/ZucchiModel for the canonical source repository
+ * @copyright Copyright (c) 2005-2013 Zucchi Limited. (http://zucchi.co.uk)
+ * @license   http://zucchi.co.uk/legals/bsd-license New BSD License
+ */
+namespace ZucchiModelTest\Behaviour\TestAsset;
+
+use ZucchiModel\Annotation as Model;
+
+/**
+ * @author Rick Nicol <rick@zucchi.co.uk>
+ * @Model\Target({"test_zucchimodel_customer","test_zucchimodel_user"})
+ */
+class Customer extends User
+{
+    /**
+     * users forename
+     *
+     * @Model\Field("string")
+     */
+    public $forename;
+
+    /**
+     * customer address
+     *
+     * @Model\Field("string")
+     */
+    public $address;
+}
