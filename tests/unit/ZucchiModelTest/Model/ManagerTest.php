8da233ab7417663603feb809791b1b4c3abd139f
Richard Nicol <richardnicol71@gmail.com>
2013-04-19 10:12:25 +0100

    Add ManagerTest.

---
+++ tests/unit/ZucchiModelTest/Model/ManagerTest.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * ZucchiModel (http://zucchi.co.uk)
+ *
+ * @link      http://github.com/zucchi/ZucchiModel for the canonical source repository
+ * @copyright Copyright (c) 2005-2013 Zucchi Limited. (http://zucchi.co.uk)
+ * @license   http://zucchi.co.uk/legals/bsd-license New BSD License
+ */
+namespace ZucchiModelTest\Model;
+
+use Codeception\Util\Stub;
+
+/**
+ * ManagerTest
+ *
+ * @author Matt Cockayne <matt@zucchi.co.uk>
+ * @author Rick Nicol <rick@zucchi.co.uk>
+ * @package ZucchiModelTest
+ * @subpackage Model
+ * @category
+ */
+class ManagerTest extends \Codeception\TestCase\Test
+{
+    /**
+     * @var \CodeGuy
+     */
+    protected $codeGuy;
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testMe()
+    {
+
+    }
+}
